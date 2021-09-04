6292106b7f5c85a6dcd3b55418e3b056afb4b9ac
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-09-05 00:49:10 +0300

    update test pacakge to contain RestAssured framework

--- src/test/java/org/closure/course/CourseApplicationTests.java
+++ src/test/java/org/closure/course/CourseApplicationTests.java
@@ -20,22 +20,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import io.restassured.http.Header;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
 @WebMvcTest(MyController.class)
 class CourseApplicationTests {
 
 	@Autowired
 	MockMvc mockMvc;
 
-	
+	@Test
+	public void TestWithAssured() throws Exception {
+		given().header(new Header("Authorization","Basic YWRtaW46YWRtaW5AMTIz")).get("admin").then().statusCode(200);
+	}
+
 	@Test
 	@WithMockUser(username = "admin", password = "admin@123", roles = "ADMIN")
-	public void basicAuthTest() throws Exception {
+	public void mytest1() throws Exception {
+		MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders.get("/admin");
 		mockMvc.perform(MockMvcRequestBuilders.get("/admin")).andExpect((e) -> {
 			assertEquals(200, e.getResponse().getStatus());
 		});
