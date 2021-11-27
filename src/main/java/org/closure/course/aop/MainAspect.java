737a1f968648e87175fc7b312de94422f469bf18
anasa <anasa@techno-visions.net>
2021-11-27 16:09:49 +0200

    init point

--- src/main/java/org/closure/course/aop/MainAspect.java
+++ src/main/java/org/closure/course/aop/MainAspect.java
@@ -1,5 +1,14 @@
 package org.closure.course.aop;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.annotation.PostConstruct;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -17,59 +26,84 @@ public class MainAspect {
     @Pointcut("execution(* org.closure.course.service.ProductService.*(..))")
     public void adviceProduct(){}
 
+    FileReader fr;
+    FileWriter fw;
+
+    @PostConstruct
+    public void init(){
+        Path path = Paths.get("logs/ProductLog.log");
+        try {
+          fr = new FileReader(path.toFile());
+            fw = new FileWriter(path.toFile(),true);
+        } catch (IOException e) {
+           System.out.println(e.getMessage());
+        }
+
+    }
     @Before("adviceProduct()")  // @After(adviceProduct)  no difference
-    public void logData(JoinPoint joinPoint){
-        System.out.println("execute : " + joinPoint.getSignature());
-        System.out.println("with following params : ");
+    public void logData(JoinPoint joinPoint) throws IOException{
+        fw.write("execute : " + joinPoint.getSignature());
+        fw.write("with following params : ");
         for(Object param : joinPoint.getArgs())
         {
             if(param instanceof Product){
                 Product value = (Product) param;
-                System.out.println(value.toString());
+                fw.write(value.toString());
             }else if(param instanceof Integer)
             {
                 Integer value = (Integer) param;
-                System.out.println(value);
+                fw.write(value);
             }
         }
+        fw.flush();
+        int c = -1;
+        String resultLog = "";
+        while ((c = fr.read())!= -1) {
+            resultLog = resultLog+((char)c);
+        }
+        System.out.println(resultLog);
     }
 
     @AfterReturning(pointcut = "execution(* org.closure.course.service.ProductService.*(..))",returning = "result")
-    public void logActualData(JoinPoint joinPoint, Object result)
+    public void logActualData(JoinPoint joinPoint, Object result) throws IOException
     {
-        System.out.println("execute : " + joinPoint.getSignature());
-        System.out.println("with following params : ");
+        fw.write("execute : " + joinPoint.getSignature());
+        fw.write("with following params : ");
         for(Object param : joinPoint.getArgs())
         {
             if(param instanceof Product){
                 Product value = (Product) param;
-                System.out.println(value.toString());
+                fw.write(value.toString());
             }else if(param instanceof Integer)
             {
                 Integer value = (Integer) param;
-                System.out.println(value);
+                fw.write(value);
             }
         }
 
-        System.out.println("result : ");
-        System.out.println(((ProductDto)result).toString());
+        fw.write("result : ");
+        fw.write(((ProductDto)result).toString());
+        fw.flush();
     }
 
     @Around("adviceProduct()")
     public Object logDetails(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
         //before
-        System.out.println("before execution");
+        fw.write("before execution");
         Object obj = proceedingJoinPoint.proceed();
         //after
-        System.out.println("after execution");
+        fw.write("after execution");
+        fw.flush();
         return obj;
     } 
 
     @AfterThrowing(pointcut = "execution(* org.closure.course.service.ProductService.*(..))",throwing = "error")
-    public void ExceptionAdvice(JoinPoint joinPoint, Throwable error)
+    public void ExceptionAdvice(JoinPoint joinPoint, Throwable error) throws IOException
     {
-        System.out.println("error handled");
-        System.out.println(error.getMessage());
+        fw.write("error handled");
+        fw.write(error.getMessage());
+        fw.flush();
+
     }
 
 }
